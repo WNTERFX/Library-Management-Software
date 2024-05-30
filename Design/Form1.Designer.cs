039102de9ef691aa55f8ed5ce43ce9c17b8d7e61
MrDough3 <161266603+MrDough3@users.noreply.github.com>
2024-05-30 19:38:26 +0800

    Reigster (Form 1) Login (Form 2)
    Signed-off-by: MrDough3 <161266603+MrDough3@users.noreply.github.com>

---
+++ Design/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Codexsyst
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            button1 = new Button();
+            label2 = new Label();
+            label3 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(67, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(153, 30);
+            label1.TabIndex = 0;
+            label1.Text = "Create account";
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.Ivory;
+            textBox1.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(48, 52);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(191, 27);
+            textBox1.TabIndex = 1;
+            textBox1.Text = "Username";
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = Color.Ivory;
+            textBox2.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox2.Location = new Point(48, 99);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(191, 27);
+            textBox2.TabIndex = 2;
+            textBox2.Text = "Student ID";
+            // 
+            // textBox3
+            // 
+            textBox3.BackColor = Color.Ivory;
+            textBox3.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox3.Location = new Point(48, 150);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(191, 27);
+            textBox3.TabIndex = 3;
+            textBox3.Text = "Email Address";
+            // 
+            // textBox4
+            // 
+            textBox4.BackColor = Color.Ivory;
+            textBox4.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox4.Location = new Point(48, 201);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(191, 27);
+            textBox4.TabIndex = 4;
+            textBox4.Text = "Password";
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.GradientActiveCaption;
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.ForeColor = SystemColors.ButtonHighlight;
+            button1.Location = new Point(79, 244);
+            button1.Name = "button1";
+            button1.Size = new Size(125, 41);
+            button1.TabIndex = 5;
+            button1.Text = "Create Account";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.ForeColor = SystemColors.ControlDarkDark;
+            label2.Location = new Point(78, 302);
+            label2.Name = "label2";
+            label2.Size = new Size(142, 15);
+            label2.TabIndex = 6;
+            label2.Text = "Already have an account?";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(126, 327);
+            label3.Name = "label3";
+            label3.Size = new Size(37, 15);
+            label3.TabIndex = 7;
+            label3.Text = "Login";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Ivory;
+            ClientSize = new Size(285, 410);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(button1);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Button button1;
+        private Label label2;
+        private Label label3;
+    }
+}
---
+++ Design/Form1.cs
@@ -0,0 +1,10 @@
+namespace Codexsyst
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
